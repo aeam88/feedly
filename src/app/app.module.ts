c00e5f641d84ee3dc3bffc3f8ab077c4ab0289ab
Alejandro Alvarez <a.alvarezmontes@gmail.com>
2018-07-08 17:03:41 -0400

    Primer commit mio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,27 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+
+import firebase from 'firebase';
+
+
+var config = {
+  apiKey: "AIzaSyDds-vQhAIm_y1jvz48IZPmZYxbf8vIUBk",
+  authDomain: "feedly-5e940.firebaseapp.com",
+  databaseURL: "https://feedly-5e940.firebaseio.com",
+  projectId: "feedly-5e940",
+  storageBucket: "feedly-5e940.appspot.com",
+  messagingSenderId: "891325014043"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +34,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
